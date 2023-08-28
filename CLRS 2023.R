ecd7f7ac02955a2ad4b736dcf9879dd7ee86d1a1
rajesh_sahasrabuddhe <rajesh.sahasrabuddhe@oliverwyman.com>
2023-08-28 16:58:45 -0400

    In Office Changes

--- CLRS 2023.R
+++ CLRS 2023.R
@@ -46,7 +46,7 @@ rstan::extract(object = clrs_2023, 'beta')$beta |> apply(MARGIN = 2,
 rstan::extract(object = clrs_2023, 'sig')$sig |> apply(MARGIN = 2, 
   FUN = mean) |> round(3)
 
-# w = 9, d = 10
+# Ultimate for 1996, w = 9, d = 10
 logprem <- train_df$EarnedPremNet_C[train_df$AccidentYear == 1996] |> 
   mean() |> log()
 ults <- rlnorm(n = 12e3, meanlog = logprem + 
